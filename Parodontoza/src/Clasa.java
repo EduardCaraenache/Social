4969076f0e675751faf81e1d143904fbcbb00bc0
Kapaceala <eduard.caraenacherot@gmail.com>
2021-11-14 10:59:17 +0200

    Initial commit

---
+++ Parodontoza/src/Clasa.java
@@ -0,0 +1,8 @@
+public class Clasa{ //declarare clasa
+
+    public static void main(String[] args) {
+        Clasa obiect = new Clasa();  // Obiectul
+    }
+    // Clasa are un nou obiect numit obiect
+    // Constructori
+}
