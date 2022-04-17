cd05b3b540d669a918d0bde30dd0dc953f2d4592
Eduuu <92028565+EduardCaraenache@users.noreply.github.com>
2022-04-17 14:48:52 +0300

    API + Client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+// import Profile from "./pages/profile/Profile";
+// import Register from "./pages/register/Register";
+
+//Asta e BUN NOUimport { Person } from "@mui/icons-material";
+//inainte era alt FROM import { Person } from "@material-ui/icons";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
